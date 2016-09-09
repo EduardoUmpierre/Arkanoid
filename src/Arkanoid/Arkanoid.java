9b08d4e1533bbee18e611b77fde5ad9f23c5c52b
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2016-09-08 23:45:19 -0300

    Projeto iniciado e SimpleJava configurado para comeÃar a desenvolver o jogo.

---
+++ src/Arkanoid/Arkanoid.java
@@ -0,0 +1,26 @@
+package Arkanoid;
+
+import com.senac.SimpleJava.Graphics.Canvas;
+import com.senac.SimpleJava.Graphics.GraphicApplication;
+
+public class Arkanoid extends GraphicApplication {
+
+	@Override
+	protected void draw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void setup() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void loop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
